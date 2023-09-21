08e110f92cca22d94202dbdf78288eb162e07f7f
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-09-21 09:40:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Your code here.
+let startX, scrollLeft;
+
+document.querySelector('.items').addEventListener('mousedown', function (e) {
+  startX = e.pageX - this.offsetLeft;
+  scrollLeft = this.scrollLeft;
+  this.style.cursor = 'grabbing';
+});
+
+document.querySelector('.items').addEventListener('mousemove', function (e) {
+  if (!startX) return;
+  const x = e.pageX - this.offsetLeft;
+  const walk = (x - startX) * 3;
+  this.scrollLeft = scrollLeft - walk;
+});
+
+document.querySelector('.items').addEventListener('mouseup', function () {
+  startX = null;
+  this.style.cursor = 'grab';
+});
